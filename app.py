fba67bc21c7bce4c05eeb775bb57bd26101cc8ed
Prakhar <prakhargarg@utexas.edu>
2019-05-24 14:14:42 -0400

    sort and get data

--- app.py
+++ app.py
@@ -1,13 +1,43 @@
 from flask import Flask
+import requests
+import json
+
 app = Flask(__name__)
 
+#######################
+# Function to grab the data from the API and return the relevant info
+## Params
+### None
+## Output
+#### { string }: JSON object
+#######################
 def getData():
-  return 'hello'
+  url = 'https://api.coinranking.com/v1/public/coin/1/history/30d'
+  try:
+    response = requests.get(url)
+    json = response.json()
+    return json['data']['history']
+  except requests.exceptions.RequestException as e:
+    print(e)
 
+#######################
+# Function to return the expected output
+## Params
+### { string }: data #sorted JSON
+## Output
+#### { string } JSON obj and result
+#######################
+def makeJSON(data):
+
+
+  return jsonObject
+
+# Main and only route in this app. This will also be the homepage
 @app.route("/")
 def index():
     data = getData()
-    return data
+    sortedData = sorted(data, key=lambda k: k['timestamp'])
+    return 'hellio'
 
 if __name__ == "__main__":
   app.run()
