995daa5778200651feda54593ae8185451083f78
Prakhar <prakhargarg@utexas.edu>
2019-05-25 12:10:19 -0400

    date formatting

--- app.py
+++ app.py
@@ -22,18 +22,88 @@ def getData():
     print(e)
 
 #######################
+# Function to return the direction, price, and change
+## Params
+### { obj }: current day object
+### { float }: price of last known day
+## Output
+#### { string, Float, string } direction, price, change
+#######################
+def getDirectionChange(object, lastPrice):
+  currentPrice = float(object['price'])
+  if (lastPrice == 'na'):
+    return 'na', currentPrice, 'na'
+
+  change = currentPrice - lastPrice
+  if (currentPrice > lastPrice):
+    return ('up', currentPrice, change)
+  if (currentPrice < lastPrice):
+    return ('down', currentPrice, change)
+  else:
+    return ('same', currentPrice, 0)
+
+#######################
+# Function to return the formatted date and weekday
+## Params
+### { string }: timestamp
+## Output
+#### { string, string }: formatted date, weekday
+#######################
+def getDate(timestamp):
+  days = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday']
+  date = datetime.fromtimestamp(timestamp / 1000).strftime('%Y-%m-%d %H:%M:%S')
+  weekdayInt = datetime.fromtimestamp(timestamp / 1000).weekday()
+  weekday = days[weekdayInt]
+
+  return (date,weekday)
+
+
+#######################
 # Function to return the expected output
 ## Params
-### { string }: data #sorted JSON
+### { string }: data #sorted and filtered JSON
 ## Output
 #### { string } JSON obj and result
 #######################
 def makeJSON(data):
   finalData = []
+  lastPrice = 'na'
+  low = 0
+  high = 0
+  # iterate through all days and build its data
   for object in data:
+    change = 0
     newObject = {}
-    newObject['date'] = datetime.fromtimestamp(object['timestamp'] / 1000).strftime('%Y-%m-%d %H:%M:%S')
-    newObject['price'] = object['price']
+    lowSinceStart = False
+    highSinceStart = False
+
+    date, day = getDate(object['timestamp'])
+
+    newObject['date'] = date
+    newObject['price'] = float(object['price'])
+
+    direction, currentPrice, change = getDirectionChange(object, lastPrice)
+    newObject['direction'] = direction
+    newObject['change'] = change
+
+    newObject['dayOfWeek'] = day
+    # on the first day set the low and high to its value
+    if (low == 0):
+      high = currentPrice
+      low = currentPrice
+      lowSinceStart = True
+      highSinceStart = True
+    elif (currentPrice > high):
+      high = currentPrice
+      highSinceStart = True
+    elif (currentPrice < low):
+      low = currentPrice
+      lowSinceStart = True
+
+    newObject['highSinceStart'] = highSinceStart
+    newObject['lowSinceStart'] = lowSinceStart
+
+    lastPrice = currentPrice
     finalData.append(newObject)
   return finalData
 
